497aa0a513ef6e33537468c84d0ad93ac1120594
SanchezIsaac <1206482576@qq.com>
2025-02-07 23:10:06 +0800

    v4

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'download']
+const section_names = ['home', 'publications', 'awards', 'download']
 
 
 window.addEventListener('DOMContentLoaded', event => {
